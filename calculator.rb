0df8891e60004b3ebb1e24d61f620cda0ff7bbc9
Ashley C <62809119+ashthedash2k@users.noreply.github.com>
2020-12-05 14:46:09 -0500

    Add files via upload

---
+++ calculator.rb
@@ -0,0 +1,28 @@
+class Calculator
+
+  def initialize(num1, num2)
+    @num1, @num2 = num1, num2
+  end
+
+  def add
+    return @num1 + @num2
+  end
+
+  def subtract
+    return @num1 - @num2
+  end
+
+  def divide
+    return @num1/@num2
+  end
+
+  def multiply
+    return @num1 * @num2
+  end
+end
+
+value = Calculator.new(4, 3)
+
+adding_value = value.add()
+
+puts "your pdocut is: #{adding_value}"
